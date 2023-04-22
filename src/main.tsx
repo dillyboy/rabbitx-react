93d850a4b7038603d816d7d2bc319b47a18a7ba4
Dilshan Liyanage <nelly.dil@hotmail.com>
2023-04-23 03:12:01 +0800

    browser router

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.scss'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.scss';
 import { store } from './redux/store'
 import { Provider } from 'react-redux';
@@ -10,9 +10,9 @@ import { Provider } from 'react-redux';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <Router>
         <App />
-      </BrowserRouter>
+      </Router>
     </Provider>
   // </React.StrictMode>,
 )
